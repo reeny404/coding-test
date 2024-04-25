9c80feb364546d07868a0a3008e1a9e0ba68a65b
hyeon <61840162+reeny404@users.noreply.github.com>
2024-04-25 17:17:00 +0900

    [level 1] Title: 내적, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,4 +1,5 @@
 function solution(a, b) {
-    return a.map((val, i) => val * b[i])
-        .reduce((a,b) => a+b);
+    return a.reduce((prevResults, currentValue, i) => {
+        return prevResults += a[i] * b[i]
+    }, 0);
 }
