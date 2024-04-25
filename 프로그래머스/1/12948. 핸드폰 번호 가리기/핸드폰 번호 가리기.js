049430e19c5d661a288411d7423ffd29426f8b8e
hyeon <61840162+reeny404@users.noreply.github.com>
2024-04-25 16:46:51 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,11 +1,3 @@
-function solution(phone_number) {
-    const index = phone_number.length;
-    const starCount = index-4;
-    
-    let answer = '';
-    for(let i=0 ; i < starCount ; i++) {
-         answer += '*'
-    }
-    
-    return answer + phone_number.substring(starCount, index);
+function solution(phone) {
+    return '*'.repeat(phone.length-4) + phone.slice(-4);
 }
