b4f693131f5524fd30ac387730e176faab7c1798
hyeon <61840162+reeny404@users.noreply.github.com>
2024-04-26 13:23:18 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,23 +1,25 @@
 function solution(n) {
-    var value = getReverse3notation(n);
+    var value = [...n.toString(3)].reverse().join('');
     
-    let answer = 0;
-    for(let i=0 ; i < value.length ; i++){
-        const current = value[i];
+    return parseInt(value, 3);
+    
+//     let answer = 0;
+//     for(let i=0 ; i < value.length ; i++){
+//         const current = value[i];
         
-        const target = 3 ** (value.length - i - 1);
-        answer += current * target;  
-    }
+//         const target = 3 ** (value.length - i - 1);
+//         answer += current * target;  
+//     }
     
-    return answer;
+//     return answer;
 }
 
-function getReverse3notation (number) {
-    const nto3notation = [];
+// function getReverse3notation (number) {
+//     const nto3notation = [];
     
-    for(let quotient = number ; quotient !== 0 ; quotient = Math.floor(quotient / 3)){
-        nto3notation.push(quotient%3);
-    }
+//     for(let quotient = number ; quotient !== 0 ; quotient = Math.floor(quotient / 3)){
+//         nto3notation.push(quotient%3);
+//     }
 
-    return nto3notation.join('');
-}
+//     return nto3notation.join('');
+// }
