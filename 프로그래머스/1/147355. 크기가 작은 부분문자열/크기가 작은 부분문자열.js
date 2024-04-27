0e436153e6bf3745ce07883756a31f2b2a86c06f
hyeon <61840162+reeny404@users.noreply.github.com>
2024-04-27 23:48:23 +0900

    Update 크기가 작은 부분문자열.js

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -2,7 +2,7 @@ function solution(t, p) {
     var answer = 0;
     
     for (let i = 0, size = p.length; i < t.length - size + 1; i++) {
-        const number = t.substring(i, i+size);
+        const number = t.substring(i, i + size);
         if(number <= p) {
             answer++;
         }
