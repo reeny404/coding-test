ccc9b0689cabacc661270de5de92dbf41f01555c
hyeon <61840162+reeny404@users.noreply.github.com>
2024-05-03 13:00:24 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,9 +1,7 @@
-function solution(array, commands) {
+function solution(array, commands) {    
     return commands.map(command => {
-        const temp = array.slice(command[0]- 1, command[1])
-            .sort((a, b) => {
-                return a - b;
-            });
-        return temp[command[2]-1];
+        const start = command[0]-1, end = command[1], n = command[2]-1,
+              results = array.slice(start, end).sort((a, b) => a-b);
+        return results[n];
     });
 }
