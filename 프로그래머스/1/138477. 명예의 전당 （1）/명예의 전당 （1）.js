51fa9b412f8d63bacb6e19477734be492439ed20
hyeon <61840162+reeny404@users.noreply.github.com>
2024-05-03 17:04:18 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,25 +1,7 @@
-function solution(k, score) {
-//     const top3 = [], answer = [];   
-//     for (let i=0; i < k; i++){
-//         top3.push(score[i])
-//         answer.push(Math.min.apply(null, top3))
-//     }
-    
-//     let min = Math.min(...top3);
-//     for(let i=k; i < score.length ; i++){
-//         const cur = score[i];
-//         if (min > cur) {
-//             answer.push(min);
-//             continue;
-//         }
-//         top3.push(cur);
-//         top3.sort((a,b) => b-a).pop();
-//         min = Math.min(...top3);
-//         answer.push(min);
-//     }
-    
-    const top3 = [], answer = [];
+function solution(k, score) {    
+    const top3 = [];
     let min = 0;
+    
     return score.map((val, i) => {
         min = Math.min(...top3);
         if (i < k) {
@@ -33,5 +15,4 @@ function solution(k, score) {
         top3.sort((a,b) => b-a).pop();
         return Math.min(...top3);
     });
-    return answer;
 }
