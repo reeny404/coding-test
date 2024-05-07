7db47ffe54cbe6650703ea502436ed47eb74f5ba
hyeon <61840162+reeny404@users.noreply.github.com>
2024-05-07 12:27:31 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,24 +1,9 @@
 
 function solution(lottos, win_nums) {
-  const cnts = getCounts(lottos, win_nums);
+    const rank = [6, 6, 5, 4, 3, 2, 1];
+    
+    const min = lottos.filter(n => win_nums.indexOf(n) !== -1).length;    
+    const zero = lottos.filter(n => n === 0).length;
 
-  return [getRank(cnts[0]), getRank(cnts[0] + cnts[1])].sort((a, b) => a - b);
-}
-
-// return [맞힌 갯수, 0인 갯수]
-function getCounts(lottos, win_nums) {
-  const results = [0, 0];
-  for (let i = 0; i < lottos.length; i++) {
-    const num = lottos[i];
-    if (num === 0) {
-      results[1] += 1;
-    } else if (win_nums.indexOf(num) !== -1) {
-      results[0] += 1;
-    }
-  }
-  return results;
-}
-
-function getRank(count) {
-    return Boolean(count) ? 7 - count : 6;    
+    return [rank[min + zero], rank[min]];
 }
