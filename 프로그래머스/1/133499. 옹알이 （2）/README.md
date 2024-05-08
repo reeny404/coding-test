121092c5b1dbd3c3667624af5ecf17a1bb836801
hyeon <61840162+reeny404@users.noreply.github.com>
2024-05-08 11:40:52 +0900

    [level 1] Title: 옹알이 (2), Time: 0.37 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/README.md
+++ 프로그래머스/1/133499. 옹알이 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 옹알이 (2) - 133499 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.42 ms
+메모리: 33.4 MB, 시간: 0.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 08일 10:29:41
+2024년 05월 08일 11:40:50
 
 ### 문제 설명
 
--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,18 +1,19 @@
-function solution(babbling) {
-  const words = ["aya", "ye", "woo", "ma"];
+function solution(babblings) {
+    const words = ["aya", "ye", "woo", "ma"];
 
-  const results = babbling.map((s) => {
-    for (let i = 0; i < words.length; i++) {
-      const array = s.split(words[i]);
-      const isOver = array.join("*").split("**").length;
-
-      if (isOver !== 1 || s.length === 0) {
-        break;
-      }
-      s = array.join(".");
-    }
-    return s;
-  });
-
-  return results.filter(s => s.replaceAll(".", "").length === 0).length;
+    return babblings.filter(babbling => {
+        let splitWord = babbling;
+        
+        for (let i = 0; i < words.length; i++) {
+            const word = words[i];
+            
+            if (babbling.split(word.repeat(2)).length !== 1) { // 연속 단어 불가
+                return false;
+            }
+            
+            splitWord = splitWord.split(word).join(' ').trim();
+        }
+        
+        return splitWord.length === 0;
+    }).length;
 }
