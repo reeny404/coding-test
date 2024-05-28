d5af89598a83c62f2e52974abed101fca78ee42a
reeny404 <happy2hyeon@kakao.com>
2024-05-28 09:32:44 +0900

    fixedArray 구현

--- sparta/FixedArray.js
+++ sparta/FixedArray.js
@@ -52,6 +52,88 @@ export class FixedArray {
     }
     return `[${string}]`;
   }
+
+  //////////////////////////////
+  //////      LEVEL 2      /////
+  //////////////////////////////
+  // 배열에서 특정 요소의 첫 번째 인덱스를 반환합니다. 요소가 없으면 -1을 반환합니다.
+  indexOf(searchElement) {
+    return this.findIndex((element) => element === searchElement);
+  }
+
+  // 배열의 각 요소에 대해 predicate 결과가 true인 요소 중 제일 첫번째 요소 1개만 반환
+  // true 가 없으면 null 반환
+  find(predicate) {
+    const index = this.findIndex(predicate);
+    if (index === -1) {
+      return null;
+    }
+    return this.#array[index];
+  }
+
+  // 배열의 각 요소에 대해 predicate 결과가 true인 요소 중 제일 첫번째 요소의 index반환
+  // true 가 없으면 -1 반환
+  findIndex(predicate) {
+    for (let i = 0; i < this.#arrayLength; i++) {
+      const element = this.#array[i];
+      if (predicate(element, i, this.#array)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  // 배열에 특정 요소가 포함되어 있는지 여부를 확인합니다. (true or false)
+  includes(searchElement) {
+    return this.indexOf(searchElement) !== -1;
+  }
+
+  //////////////////////////////
+  //////      LEVEL 3      /////
+  //////////////////////////////
+  // 배열의 각 요소에 대해 제공된 함수를 한 번씩 실행합니다.
+  forEach(callback) {
+    for (let i = 0; i < this.getLength(); i++) {
+      callback(this.#array[i], i, this.#array);
+    }
+  }
+
+  // 배열의 각 요소에 대해 predicate 결과가 true인 요소를 모아 새로운 배열 반환
+  filter(predicate) {
+    const filteredValues = {};
+    for (let i = 0; i < this.getLength(); i++) {
+      const element = this.#array[i];
+      if (predicate(element, i, this.#array)) {
+        filteredValues[i] = element;
+      }
+    }
+
+    const values = Object.values(filteredValues);
+    const newArray = new FixedArray(values.length);
+    for (const val of values) {
+      newArray.push(val);
+    }
+    return newArray;
+  }
+
+  // 배열의 각 요소에 대해 callback 함수를 호출한 결과를 모아 새로운 배열로 반환
+  map(callback) {
+    const newArray = new FixedArray(this.getLength());
+    for (let i = 0; i < this.getLength(); i++) {
+      const element = callback(this.#array[i], i, this.#array);
+      newArray.push(element);
+    }
+    return newArray;
+  }
+
+  // 배열의 각 요소에 대해 제공된 함수를 호출하여 누산기에 값을 축적
+  reduce(callback, initValue = 0) {
+    let value = initValue;
+    for (let i = 0; i < this.getLength(); i++) {
+      value = callback(value, this.#array[i], i, this.#array);
+    }
+    return value;
+  }
 }
 
 // const a = new FixedArray(3);
--- sparta/FixedArray.test.js
+++ sparta/FixedArray.test.js
@@ -16,14 +16,66 @@ describe("FixedArray", () => {
       array.push(i);
     }
   });
-
-  it("getLength 테스트", () => {
+  it("getLength", () => {
     expect(fixedArray.getLength()).to.equal(array.length);
   });
-  it("stringify 테스트", () => {
+  it("stringify", () => {
     expect(fixedArray.stringify()).to.equal(JSON.stringify(array));
   });
-  it("pop 테스트", () => {
+  it("indexOf", () => {
+    expect(fixedArray.indexOf(3)).to.equal(array.indexOf(3));
+  });
+  it("find", () => {
+    const p1 = (element) => element === 3;
+    expect(fixedArray.find(p1)).to.equal(array.find(p1));
+
+    const p2 = (element, index) => element !== index;
+    expect(fixedArray.find(p2)).to.equal(null);
+    expect(array.find(p2)).to.equal(undefined);
+  });
+  it("findIndex", () => {
+    const p1 = (element) => element === 3;
+    expect(fixedArray.findIndex(p1)).to.equal(array.findIndex(p1));
+
+    const p2 = (element, index) => element !== index;
+    expect(fixedArray.findIndex(p2)).to.equal(array.findIndex(p2));
+  });
+  it("includes", () => {
+    expect(fixedArray.includes(0)).to.equal(array.includes(0));
+    expect(fixedArray.includes(1)).to.equal(array.includes(1));
+    expect(fixedArray.includes(2)).to.equal(array.includes(2));
+    expect(fixedArray.includes(3)).to.equal(array.includes(3));
+    expect(fixedArray.includes(4)).to.equal(array.includes(4));
+    expect(fixedArray.includes(-1)).to.equal(array.includes(-1));
+  });
+  it("forEach", () => {
+    const func = (v, i) => console.log(`foreach ${i}번째 ${v}`);
+    fixedArray.forEach(func);
+    console.log("--------");
+    array.forEach(func);
+  });
+  it("filter", () => {
+    const func = (v, i) => v + i > 3;
+    expect(fixedArray.filter(func).stringify()).to.equal(
+      JSON.stringify(array.filter(func))
+    );
+  });
+  it("map", () => {
+    const func = (v, i) => v + i;
+    expect(fixedArray.map(func).stringify()).to.equal(
+      JSON.stringify(array.map(func))
+    );
+  });
+  it("reduce", () => {
+    const func = (a, b) => a + b;
+    expect(fixedArray.reduce(func)).to.equal(array.reduce(func));
+  });
+  it("pop", () => {
+    expect(fixedArray.pop()).to.equal(array.pop());
+    expect(fixedArray.pop()).to.equal(array.pop());
+    expect(fixedArray.pop()).to.equal(array.pop());
+    expect(fixedArray.pop()).to.equal(array.pop());
+    expect(fixedArray.pop()).to.equal(array.pop());
     expect(fixedArray.pop()).to.equal(array.pop());
   });
 });
