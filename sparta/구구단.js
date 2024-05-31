9f846bc58051c5056fb0f04f55fa06241f9caa20
reeny404 <happy2hyeon@kakao.com>
2024-05-31 19:05:44 +0900

    구구단

---
+++ sparta/구구단.js
@@ -0,0 +1,10 @@
+function gugudan(n) {
+  for (let i = 1; i < 10; i++) {
+    console.log(`${n} * ${i} = ${n * i}`);
+  }
+}
+
+for (let i = 1; i < 10; i++) {
+  gugudan(i);
+  console.log("==================");
+}
